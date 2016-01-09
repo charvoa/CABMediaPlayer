ed8ef959e94b5ac739036cbc6e3ae3edee95574d
louis audibert <louis.audibert@epitech.eu>
2016-01-09 17:32:08 +0100

    Added Dialog1 Window to select server

---
+++ client/WindowsApplication1/Dialog1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Windows.Forms
+
+Public Class Dialog1
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBoxPort.TextChanged
+
+    End Sub
+
+    Private Sub TextBoxIP_TextChanged(sender As Object, e As EventArgs) Handles TextBoxIP.TextChanged
+
+    End Sub
+End Class
