918ebcf74964c3b61aba93efe13b171ebd702f1f
joris bertomeu <joris.bertomeu@epitech.eu>
2016-01-11 13:41:07 +0100

    Adding drag and drop to client

--- client/WindowsApplication1/Form1.vb
+++ client/WindowsApplication1/Form1.vb
@@ -44,4 +44,23 @@
         Private Sub ToolStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
 
         End Sub
-    End Class
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effect = DragDropEffects.All
+        End If
+    End Sub
+
+    Private Sub AxWindowMediaPlayer1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            Dim files() As String
+            Dim i As Integer
+
+            files = e.Data.GetData(DataFormats.FileDrop)
+            For i = 0 To files.Length - 1
+                AxWindowsMediaPlayer1.currentPlaylist.appendItem(AxWindowsMediaPlayer1.newMedia(files(i)))
+            Next
+            AxWindowsMediaPlayer1.Ctlcontrols.play()
+        End If
+    End Sub
+End Class
